5331a9b0e2bd926cac40cafd8fb325a2796d4028
Stanley Gu <stanleygu@gmail.com>
2013-01-22 19:00:23 -0800

    using twitter bootstrap for page layouts

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
+app.get('/about', routes.about);
 app.get('/users', user.list);
 app.post('/sbml2matlab', sbml2matlab.translate)
 
--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,7 @@
 exports.index = function(req, res){
   res.render('index', { title: 'sbml2matlab' });
 };
+
+exports.about = function(req, res){
+  res.render('about', { title: 'sbml2matlab' });
+};
